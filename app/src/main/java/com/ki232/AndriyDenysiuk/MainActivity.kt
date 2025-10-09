0d6684457019ddf63bddd0b93d264fc70fd5c8df
Andriy <Andrjm09@gmail.com>
2025-10-09 17:10:47 +0300

    Лабораторна робота №4

--- app/src/main/java/com/ki232/AndriyDenysiuk/MainActivity.kt
+++ app/src/main/java/com/ki232/AndriyDenysiuk/MainActivity.kt
@@ -1,32 +1,50 @@
 package com.ki232.AndriyDenysiuk
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    val switch = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            val text = it.data?.getStringExtra("second_name")
+            findViewById<TextView>(R.id.textview).setText(text)
+            Toast.makeText(this, "Success", Toast.LENGTH_LONG).show()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }*/
-        findViewById<Button>(R.id.button).setOnClickListener {
-            val text = findViewById<EditText>(R.id.edittext).text.toString()
-            val textView = findViewById<TextView>(R.id.textview)
-            if (text.isEmpty())
-                textView.setText("Введіть текст!")
-            else
-                textView.setText(text)
-            findViewById<ImageView>(R.id.imageview).setImageDrawable(resources.getDrawable(R.drawable.image))
         }
+
+        val edittext = findViewById<EditText>(R.id.edittext);
+        val textView = findViewById<TextView>(R.id.textview);
+        val button = findViewById<Button>(R.id.button);
+
+        button.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            if (!edittext.text.toString().isEmpty())
+                intent.putExtra("name", edittext.text.toString())
+            else
+                textView.setText("Введіть текст!")
+            switch.launch(intent)
+        }
+        Toast.makeText(this, "MainActivity: onCreate()", Toast.LENGTH_SHORT).show()
+
         printMe()
         var a = 3
         var b = 0.33
@@ -64,5 +82,34 @@ class MainActivity : AppCompatActivity() {
     }
     fun printMe(){
         println("Hello, Andriy Denysiuk from KI-23-2!")
+    }
+    override fun onStart(){
+        super.onStart()
+        Toast.makeText(this, "MainActivity: onStart()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "MainActivity: onResume()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "MainActivity: onPause()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "MainActivity: onStop()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "MainActivity: onDestroy()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "MainActivity: onRestart()", Toast.LENGTH_SHORT).show()
     }
 }
